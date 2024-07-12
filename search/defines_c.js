17a9415b9a1fdc4509271e2197b769a6e7c86e60
tcfshcrw <tcfshcrw@users.noreply.github.com>
2024-07-12 22:20:09 +0000

    deploy: 2fcd6d2bda797da4db242c54a38fcd7c07dde449

--- search/defines_c.js
+++ search/defines_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['sensorless_5fhoming_0',['SENSORLESS_HOMING',['../Main_8h.html#a1f1af256d94506c5dfa26a51ab56069a',1,'Main.h']]],
-  ['stepper_5fwith_5flimits_5fsensorless_5fcurrent_5fthreshold_5fin_5fpercent_1',['STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT',['../Dev_2StepperWithLimits_8cpp.html#a63f6f0d2dd0bf73350f4182e64e70e37',1,'STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT():&#160;StepperWithLimits.cpp'],['../Main_2StepperWithLimits_8cpp.html#a63f6f0d2dd0bf73350f4182e64e70e37',1,'STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT():&#160;StepperWithLimits.cpp']]],
-  ['steppinstepper_2',['stepPinStepper',['../Main_8h.html#a48d6538f9e2e592b98dcd0f97ceaa039',1,'Main.h']]]
+  ['serial_5fcoomunication_5ftask_5fdelay_5fin_5fms_1',['SERIAL_COOMUNICATION_TASK_DELAY_IN_MS',['../Main_8h.html#a09383e3355dd2c33c604b4870ac4e101',1,'Main.h']]],
+  ['stepper_5fwith_5flimits_5fsensorless_5fcurrent_5fthreshold_5fin_5fpercent_2',['STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT',['../Dev_2StepperWithLimits_8cpp.html#a63f6f0d2dd0bf73350f4182e64e70e37',1,'STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT():&#160;StepperWithLimits.cpp'],['../Main_2StepperWithLimits_8cpp.html#a63f6f0d2dd0bf73350f4182e64e70e37',1,'STEPPER_WITH_LIMITS_SENSORLESS_CURRENT_THRESHOLD_IN_PERCENT():&#160;StepperWithLimits.cpp']]],
+  ['steppinstepper_3',['stepPinStepper',['../Main_8h.html#a48d6538f9e2e592b98dcd0f97ceaa039',1,'Main.h']]]
 ];
